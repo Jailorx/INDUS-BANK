e8a9f55f73d9b05f12ec422782bb7e8363dfb328
Shubham Bisht <shubhambisht3010@gmail.com>
2025-07-27 02:16:25 +0530

    -Fix package name (Controllers->controller) -Add Customized security config for endpoints

---
+++ src/main/java/com/bank/IndusBank/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.bank.IndusBank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) ->
+            requests.requestMatchers("/v1/account","/v1/balance","/v1/loans","/v1/cards").authenticated()
+                    .requestMatchers("/v1/notices","/v1/contact","/error").permitAll()
+        );
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
+    }
+}
