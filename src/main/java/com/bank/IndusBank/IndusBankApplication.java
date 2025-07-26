e0bfed0b531fb9df666a81728f94208418a41b50
Shubham Bisht <shubhambisht3010@gmail.com>
2025-07-27 01:17:04 +0530

    Project Initialization

---
+++ src/main/java/com/bank/IndusBank/IndusBankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.IndusBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndusBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndusBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/IndusBank/IndusBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.IndusBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndusBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
